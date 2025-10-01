9ca9bbc6ab3d9adce99eb5f65a671170d960f6f5
unknown <sr0589071@gmail.com>
2025-10-01 21:08:43 +0600

    create an jwt secret

---
+++ src/utils/jwt.ts
@@ -0,0 +1,15 @@
+import jwt, { Secret, SignOptions } from "jsonwebtoken";
+
+const JWT_SECRET: Secret = process.env.JWT_SECRET || "supersecret";
+
+export const signToken = (
+  payload: object,
+  expiresIn: string | number = "1h"
+): string => {
+  const options: SignOptions = { expiresIn: expiresIn as any };
+  return jwt.sign(payload, JWT_SECRET, options);
+};
+
+export const verifyToken = (token: string): any => {
+  return jwt.verify(token, JWT_SECRET);
+};
