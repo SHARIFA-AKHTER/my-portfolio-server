75dceee354aa2977d9db050a3f291b3b6be350ab
unknown <sr0589071@gmail.com>
2025-10-01 21:14:12 +0600

    added an auth.ts file

---
+++ src/middleware/auth.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../utils/jwt";
+
+export interface AuthRequest extends Request {
+  user?: any;
+}
+
+const authMiddleware = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      success: false,
+      message: "No token provided",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded; // attach user info to request
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
+};
+
+export default authMiddleware;
