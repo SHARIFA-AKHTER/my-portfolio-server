582dedc1272522aaa4c4862904a923b271307c3a
unknown <sr0589071@gmail.com>
2026-01-14 18:54:04 +0600

    updated an authWithGoogle

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { signToken } from "../../utils/jwt";
 import { comparePassword, hashPassword } from "../../utils/bcrypt";
 import prisma from "../../config/db";
+import { OAuth2Client } from 'google-auth-library';
 
 interface LoginPayload {
   email: string;
@@ -48,43 +49,89 @@ const loginWithEmailAndPassword = async (payload: LoginPayload) => {
 };
 
 // Google Auth (Basic)
-const authWithGoogle = async (payload: GooglePayload) => {
-  const { idToken } = payload;
+// const authWithGoogle = async (payload: GooglePayload) => {
+//   const { idToken } = payload;
 
-  if (!idToken) {
-    throw new Error("Google idToken required!");
-  }
+//   if (!idToken) {
+//     throw new Error("Google idToken required!");
+//   }
 
-  const googleEmail = "googleuser@example.com";
-  const googleName = "Google User";
+//   const googleEmail = "googleuser@example.com";
+//   const googleName = "Google User";
 
-  // check user exists
-  let user = await prisma.user.findUnique({
-    where: { email: googleEmail },
+//   // check user exists
+//   let user = await prisma.user.findUnique({
+//     where: { email: googleEmail },
+//   });
+
+//   if (!user) {
+//     user = await prisma.user.create({
+//       data: {
+//         email: googleEmail,
+//         name: googleName,
+//         password: await hashPassword("google-auth"),
+//         phone: "01315909090",
+//       },
+//     });
+//   }
+
+//   const token = signToken({ id: user.id, email: user.email }, "1d");
+
+//   return {
+//     success: true,
+//     message: "Google auth successful",
+//     token,
+//     user: {
+//       id: user.id,
+//       name: user.name,
+//       email: user.email,
+//     },
+//   };
+// };
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+export const authWithGoogle = async (payload: any) => {
+  const { token } = payload;
+
+  if (!token) throw new Error("Google token missing");
+
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGLE_CLIENT_ID,
   });
+
+  const google = ticket.getPayload();
+
+  const email = google?.email;
+  const name =
+    google?.name ||
+    google?.email?.split("@")[0] ||
+    "Google User";
+
+  const picture = google?.picture || null;
+
+  if (!email) throw new Error("Google email not found");
+
+  let user = await prisma.user.findUnique({ where: { email } });
 
   if (!user) {
     user = await prisma.user.create({
       data: {
-        email: googleEmail,
-        name: googleName,
-        password: await hashPassword("google-auth"),
-        phone: "01315909090",
+        email,
+        name,
+        avatar: picture,
+        password: "google-auth",
       },
     });
   }
 
-  const token = signToken({ id: user.id, email: user.email }, "1d");
+  const jwt = signToken({ id: user.id, email: user.email }, "7d");
 
   return {
     success: true,
-    message: "Google auth successful",
-    token,
-    user: {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-    },
+    token: jwt,
+    user,
   };
 };
 
